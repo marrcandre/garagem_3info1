8aa3e08f533d2773957cf596d7ca3e2a494bbdf0
Marco André Mendes <marcoandre@gmail.com>
2022-06-29 15:00:47 -0300

    instalação do black

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Marca(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.nome
+        return self.nome.upper()
 
 
 class Categoria(models.Model):
